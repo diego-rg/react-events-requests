1f262f7d72baa0aed1e420d43bc59ca2e9ff0412
diego-rg <76407123+diego-rg@users.noreply.github.com>
2022-03-04 18:58:15 +0100

    event handlers

---
+++ src/components/SearchBar.js
@@ -0,0 +1,30 @@
+import React from "react";
+
+//Como necesitamos usar state, usamos compoñente de clase (máis adiante hooks, redux)
+//Creamos evento onchange no input
+class SearchBar extends React.Component {
+  onInputChange(event) {
+
+  }
+
+  render() {
+    return (
+      <div className="ui segment search-bar">
+        <form className="ui form search-bar__form" action="">
+          <div className="field search-bar__field">
+            <label className="search-bar__field" htmlFor="">
+              Buscar:
+            </label>
+            <input
+              className="search-bar__input"
+              type="text"
+              onChange={this.onInputChange}//Usando this.onInputChange() executaríase ao renderizar. O que queremos é que o faga cando o user fai input
+            />
+          </div>
+        </form>
+      </div>
+    );
+  }
+}
+
+export default SearchBar;
