c15adf4901323fd01fd38f93ceb51b29868d2c8e
diego-rg <76407123+diego-rg@users.noreply.github.com>
2022-03-07 22:33:16 +0100

    axios: get

--- src/components/App.js
+++ src/components/App.js
@@ -5,7 +5,12 @@ import SearchBar from "./SearchBar";
 //Refactor a compoñente de clase para pasar o query do compoñente SearchBar a App (de fillo a pai)
 class App extends React.Component {
   onSearchSubmit(query) {
-    console.log(query);
+    axios.get("https://api.unsplash.com/search/photos", {
+      params: { query: query },
+      headers: {
+        Authorization: "Client-ID jPKt0eNaMazgp0Td0TGdlkmDmKIFveXjQAHwUAqIAqs"
+      }
+    })
   }
 
   render() {
