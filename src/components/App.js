67361f130d0efa065819ec635488dc4a8a1559d9
diego-rg <76407123+diego-rg@users.noreply.github.com>
2022-03-08 18:31:47 +0100

    async/await vs .then with axios promises

--- src/components/App.js
+++ src/components/App.js
@@ -3,20 +3,26 @@ import axios from "axios";
 import SearchBar from "./SearchBar";
 
 //Refactor a compoñente de clase para pasar o query do compoñente SearchBar a App (de fillo a pai)
+//A api de unsplash require rexistro e clave. Axios devolve unha promise
 class App extends React.Component {
-  onSearchSubmit(query) {
-    axios.get("https://api.unsplash.com/search/photos", {
+  state = { images: [] }
+
+  onSearchSubmit = async (query) => {
+    const response = await axios.get("https://api.unsplash.com/search/photos", {
       params: { query: query },
       headers: {
         Authorization: "Client-ID jPKt0eNaMazgp0Td0TGdlkmDmKIFveXjQAHwUAqIAqs"
       }
     })
+    this.setState({ images: response.data.results })
   }
+//alternativa con .then((res) => {console.log(res)}) para traballar coa promise en vez de async await
 
   render() {
     return (
       <div className="ui container app-container">
         <SearchBar onSubmitQuery={this.onSearchSubmit} />
+        Resultados: { this.state.images.length } imágenes
       </div>
     );
   }
