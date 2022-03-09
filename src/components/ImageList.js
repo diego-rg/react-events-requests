dd4da4808622f1092a6e3b80fad0c69c287856ac
diego-rg <76407123+diego-rg@users.noreply.github.com>
2022-03-09 16:25:09 +0100

    keys and destrcturing

--- src/components/ImageList.js
+++ src/components/ImageList.js
@@ -1,8 +1,8 @@
 import React from "react";
 
 const ImageList = (props) => {
-    const images = props.images.map((img) => {
-        return <img src={img.urls.regular} alt="" />
+    const images = props.images.map(({ description, id, urls }) => {
+        return <img src={urls.regular} alt={description} key={id} />
     });
     return <div>{images}</div>
 }
